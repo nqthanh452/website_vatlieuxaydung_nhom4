e995e5402648b193f173c7dd1798f34488bcef7f
nqthanh452 <nqthanh452@gmail.com>
2021-09-17 22:16:03 +0700

    du lieu moi

---
+++ products.php
@@ -0,0 +1,183 @@
+<?php
+	include 'inc/header.php';
+	include 'inc/slider.php';
+ ?>
+
+
+<div class="main">
+    <div class="content">
+    	<div class="content_top">
+    		<div class="heading">
+    		<h3>Gạch</h3>
+    		</div>
+    		<div class="clear"></div>
+    	</div>
+	    <div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					 <div class="img-product"><img src="images/gach-ong-thanh-tam.png" alt="" /></div>
+					 <h2>Gạch ống Thành Tâm</h2>
+					 <p>Quy cách: 8x8x18</p>
+					 <p><span class="price">1.090 đồng/viên</span></p>
+				    <div class="button"><span><a href="details.php" class="details">Chi tiêt</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					 <div class="img-product"><img src="images/gopt.jpg" alt="" /></div>
+					 <h2>Gạch ống Phước Thành</h2>
+					 <p>Quy cách: 8x8x18</p>
+					 <p><span class="price">1.090 đồng/viên</span></p>
+				    <div class="button"><span><a href="details.php" class="details">Chi tiêt</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					 <div class="img-product"><img src="images/gdtt.jpg" alt="" /></div>
+					 <h2>Gạch đinh Thành Tâm</h2>
+					 <p>Quy cách: 8x8x18</p>
+					 <p><span class="price">1.090 đồng/viên</span></p>
+				    <div class="button"><span><a href="details.php" class="details">Chi tiêt</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					 <div class="img-product"><img src="images/gdtt.jpg" alt="" /></div>
+					 <h2>Gạch đinh Phước Thành</h2>
+					 <p>Quy cách: 8x8x18</p>
+					 <p><span class="price">1.090 đồng/viên</span></p>
+				    <div class="button"><span><a href="details.php" class="details">Chi tiêt</a></span></div>
+				</div>			
+			</div>
+		<div class="content_bottom">
+    		<div class="heading">
+    		<h3>Đá</h3>
+    		</div>
+    		<div class="clear"></div>
+    	</div>
+		<div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					<div class="img-product"><img src="images/da12xanh.jpg" alt="" /></div>
+					 <h2>Đá 1x2 xanh </h2>
+					 <p>Đơn vị tính: M3</p>
+					 <p><span class="price">292.000 đồng/M3</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Chi tiết</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<div class="img-product"><img src="images/da12den.jpg" alt="" /></div>
+					 <h2>Đá 1x2 đen </h2>
+					 <p>Đơn vị tính: M3</p>
+					 <p><span class="price">420.000 đồng/M3</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Chi tiết</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<div class="img-product"><img src="images/damibui.jpg" alt="" /></div>
+					 <h2>Đá mi bụi </h2>
+					 <p>Đơn vị tính: M3</p>
+					 <p><span class="price">280.000 đồng/M3</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Chi tiết</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<div class="img-product"><img src="images/damisang.jpg" alt="" /></div>
+					 <h2>Đá mi sàng </h2>	
+					 <p>Đơn vị tính: M3</p>			 
+					 <p><span class="price">270.000 đồng/M3</span></p>   
+				     <div class="button"><span><a href="details.php" class="details">Chi tiết</a></span></div>
+				</div>
+				<!-- <div class="grid_1_of_4 images_1_of_4">
+					<div class="img-product"><img src="images/da46.jpg" alt="" /></div>
+					 <h2>Đá 4x6 </h2>	
+					 <p>Đơn vị tính: M3</p>			 
+					 <p><span class="price">290.000 đồng/M3</span></p>   
+				     <div class="button"><span><a href="details.php" class="details">Chi tiết</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<div class="img-product"><img src="images/da57.jpg" alt="" /></div>
+					 <h2>Đá 5x7 </h2>	
+					 <p>Đơn vị tính: M3</p>			 
+					 <p><span class="price">295.000 đồng/M3</span></p>   
+				     <div class="button"><span><a href="details.php" class="details">Chi tiết</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<div class="img-product"><img src="images/da04loai1.jpg" alt="" /></div>
+					 <h2>Đá 0x4 loại I </h2>	
+					 <p>Đơn vị tính: M3</p>			 
+					 <p><span class="price">260.000 đồng/M3</span></p>   
+				     <div class="button"><span><a href="details.php" class="details">Chi tiết</a></span></div>
+				</div> -->
+			</div>
+			<div class="content_bottom">
+    		<div class="heading">
+    		<h3>Xi măng</h3>
+    		</div>
+    		<div class="clear"></div>
+    	</div>
+			<div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					<div class="img-product"><img src="images/Xi măng Hoàng Long PCB30.jpg" alt="" /></div>
+					 <h2>Xi măng Hoàng Long PCB30</h2>
+					 <p>Đơn giá: tấn</p>
+					 <p><span class="price">900.000</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Chi tiết</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<div class="img-product"><img src="images/Xi măng Hoàng Long PCB40.jpg" alt="" /></div>
+					 <h2>Xi măng Hoàng Long PCB30 </h2>
+					 <p>Đơn giá: tấn</p>
+					 <p><span class="price">950.000 </span></p>
+				     <div class="button"><span><a href="details.php" class="details">Chi tiết</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<div class="img-product"><img src="images/Xi măng Vissai PCB40.jpg" alt="" /></div>
+					 <h2>Xi măng Vissai PCB40 </h2>
+					 <p>Đơn giá: tấn</p>
+					 <p><span class="price">1.000.000 </span></p>
+				     <div class="button"><span><a href="details.php" class="details">Chi tiết</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<div class="img-product"><img src="images/Xi măng Vissai PCB30.jpg" alt="" /></div>
+					 <h2>Xi măng Vissai PCB30 </h2>	
+					 <p>Đơn giá: tấn</p>			 
+					 <p><span class="price">930.000</span></p>   
+				     <div class="button"><span><a href="details.php" class="details">Chi tiết</a></span></div>
+				</div>
+			</div>
+			<div class="content_bottom">
+    		<div class="heading">
+    		<h3>Thép</h3>
+    		</div>
+    		<div class="clear"></div>
+    	</div>
+			<div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					<div class="img-product"><img src="images/Thép Việt Nhật phi 10.jpg" alt="" /></div>
+					 <h2>Thép Việt Nhật phi 10 </h2>
+					 <p>Đơn vị tính: Cây ( độ dài 11.7m)</p>
+					 <p><span class="price">65.000</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Chi tiết</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<div class="img-product"><img src="images/Thép Việt Nhật phi 18.jpg" alt="" /></div>
+					 <h2>Thép Việt Nhật phi 18 </h2>
+					 <p>Đơn vị tính: Cây ( độ dài 11.7m</p>
+					 <p><span class="price">243.000</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Chi tiết</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<div class="img-product"><img src="images/Thép cuộn Việt Nhật phi 8.jpg" alt="" /></div>
+					 <h2>Thép cuộn Việt Nhật phi 8 </h2>
+					 <p>Đơn vị tính: Kg</p>
+					 <p><span class="price">9000</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Chi tiết</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<div class="img-product"><img src="images/Thép cuộn Việt Nhật phi 6.jpg" alt="" /></div>
+					 <h2>Thép cuộn Việt Nhật phi 6 </h2>	
+					 <p>Đơn vị tính: Kg</p>			 
+					 <p><span class="price">9000</span></p>   
+				     <div class="button"><span><a href="details.php" class="details">Chi tiết</a></span></div>
+				</div>
+				
+			</div>
+
+    </div>
+ </div>
+</div>
+ 
+<?php 
+	include 'inc/footer.php';
+?>
+
