61437af2abe5d2bf9a34ecc923dd17c0ebf07ec9
Na Vu <vuthilyna304@gmail.com>
2021-09-28 19:43:40 +0700

    change css

--- index.php
+++ index.php
@@ -12,28 +12,28 @@
     	</div>
 	      <div class="section group">
 				<div class="grid_1_of_4 images_1_of_4">
-					 <a href="details.php"><img src="images/cat-xay-dung.jpg" alt="" /></a>
+					 <a href="details.php"><div class='img-product'><img src="images/cat-xay-dung.jpg" alt="" /></div></a>
 					 <h2>Cát xây dựng </h2>
 					 <p>Cát xây dựng giá rẻ đảm bảo tiêu chuẩn chất lượng. Các loại cát san lấp, cát xây tô, cát bê tông</p>
 					 <p><span class="price">150.000đ/1m3</span></p>
 				     <div class="button"><span><a href="details.php" class="details">Chi tiết</a></span></div>
 				</div>
 				<div class="grid_1_of_4 images_1_of_4">
-					<a href="details.php"><img src="images/bot-cha-ron-1.jpg" alt="" /></a>
+					<a href="details.php"><div class='img-product'><img src="images/bot-cha-ron-1.jpg" alt="" /></div></a>
 					 <h2>BỘT CHÀ RON </h2>
 					 <p> KHÁNG KHUẨN GROUTEX, 5KG, GRO0004H-H+</p>
 					 <p><span class="price">156.000/1 bao</span></p>   
 				     <div class="button"><span><a href="details.php" class="details">Chi tiết</a></span></div>
 				</div>
 				<div class="grid_1_of_4 images_1_of_4">
-					<a href="details.php"><img src="images/gach-kinh-kim-cuong.jpg" alt="" /></a>
+					<a href="details.php"><div class='img-product'><img src="images/gach-kinh-kim-cuong.jpg" alt="" /></div></a>
 					 <h2>Gạch kính kim cương </h2>
 					 <p>Tô điểm cho không gian sống hiện đại</p>
 					 <p><span class="price">$220.97</span></p> 
 				     <div class="button"><span><a href="details.php" class="details">Chi tiết</a></span></div>
 				</div>
 				<div class="grid_1_of_4 images_1_of_4">
-					<img src="images/ngoinoc_cogo.jpg" alt="" />
+					<a href="details.php"><div class='img-product'><img src="images/ngoinoc_cogo.jpg" alt="" /></div></a>
 					 <h2>Ngói nóc có gờ </h2>
 					 <p>dùng để lợp mái nhà, chất lượng cao</p>
 					 <p><span class="price">$415.54</span></p>  
@@ -48,25 +48,25 @@
     	</div>
 			<div class="section group">
 				<div class="grid_1_of_4 images_1_of_4">
-					 <a href="details.php"><img src="images/ton-song-ngoi-phuong-nam.jpg" alt="" /></a>
+					<a href="details.php"><div class='img-product'><img src="images/ton-song-ngoi-phuong-nam.jpg" alt="" /></div></a>
 					 <h2>Tôn Giả Ngói Phương Nam </h2>
 					 <p><span class="price">$403.66</span></p>
 				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
 				</div>
 				<div class="grid_1_of_4 images_1_of_4">
-					<a href="details.php"><img src="images/xi-mang-nghi-son.jpg" alt="" /></a>
+					<a href="details.php"><div class='img-product'><img src="images/xi-mang-nghi-son.jpg" alt="" /></div></a>
 					 <h2>Xi măng Nghi Sơn </h2>
 					 <p><span class="price">72.000đ</span></p> 
 				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
 				</div>
 				<div class="grid_1_of_4 images_1_of_4">
-					<a href="details.php"><img src="images/gdtt.jpg" alt="" /></a>
+					<a href="details.php"><div class='img-product'><img src="images/gdtt.jpg" alt="" /></div></a>
 					 <h2>Gach đinh Thành Tâm </h2>
 					 <p><span class="price">1.090 đồng/viên</span></p>
 				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
 				</div>
 				<div class="grid_1_of_4 images_1_of_4">
-				 <img src="images/gach-granite.jpg" alt="" />
+					<a href="details.php"><div class='img-product'><img src="images/gach-granite.jpg" alt="" /></div></a>
 					 <h2>GẠCH GRANITE 80802 </h2>					 
 					 <p><span class="price">595.000 vnđ/ m2</span></p>
 
